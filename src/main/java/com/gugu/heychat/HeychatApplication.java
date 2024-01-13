e917d7a8cad2635438eefd80742483c37e0c27ec
soomin <soomni20@gmail.com>
2024-01-13 11:27:05 +0900

    프로젝트 생성

---
+++ src/main/java/com/gugu/heychat/HeychatApplication.java
@@ -0,0 +1,13 @@
+package com.gugu.heychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeychatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeychatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gugu/heychat/HeychatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gugu.heychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeychatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
